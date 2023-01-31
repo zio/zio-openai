add0a8b7d993580103968535e7604d928f707a8e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2023-01-31 09:08:25 +0100

    Use zio-schema 0.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies {
   val zioJson = "0.4.2"
   val zioNio = "2.0.0"
   val zioPrelude = "1.0.0-RC16"
-  val zioSchema = "0.4.6+2-a471f726+20230130-1929-SNAPSHOT"
+  val zioSchema = "0.4.7"
 }
