51abf262d93ae6969001dc12c57a88ac19e69724
Scala Steward <scala_steward@virtuslab.com>
2023-03-19 01:35:03 +0000

    Update zio-json to 0.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Dependencies {
 
   val zio = "2.0.10"
   val zioHttp = "0.0.5"
-  val zioJson = "0.4.2"
+  val zioJson = "0.5.0"
   val zioNio = "2.0.1"
   val zioPrelude = "1.0.0-RC18"
   val zioSchema = "0.4.8"
