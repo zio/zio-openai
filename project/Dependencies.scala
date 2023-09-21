1d1eec8130d58eaa27c72e2a6fb72df3086f7b93
Scala Steward <scala_steward@virtuslab.com>
2023-09-21 02:19:45 +0000

    Update zio-prelude to 1.0.0-RC21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   val zioHttp = "3.0.0-RC2"
   val zioJson = "0.5.0"
   val zioNio = "2.0.1"
-  val zioPrelude = "1.0.0-RC19"
+  val zioPrelude = "1.0.0-RC21"
   val zioSchema = "0.4.12"
 }
