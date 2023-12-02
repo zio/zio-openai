d5a4b93480733c96a65248ed6d4ca1ac89e392f4
Scala Steward <scala_steward@virtuslab.com>
2023-12-02 05:31:25 +0000

    Update zio-http to 3.0.0-RC4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 object Dependencies {
 
   val zio = "2.0.19"
-  val zioHttp = "3.0.0-RC3"
+  val zioHttp = "3.0.0-RC4"
   val zioJson = "0.6.2"
   val zioNio = "2.0.2"
   val zioPrelude = "1.0.0-RC21"
